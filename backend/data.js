14d60ef021bbf41026422d9ade0c826f5b78c2a7
Vina Compson <vv.compson@icloud.com>
2018-12-17 12:19:42 +1100

    server and app files

---
+++ backend/data.js
@@ -0,0 +1,15 @@
+//backend/data.js
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//this will be our data base's data structure
+const DataSchema = new Schema(
+    {
+        id: Number,
+        message: String
+    }, 
+        { timestamps: true }
+)
+
+module.exports = mongoose.model("Data", DataSchema);
